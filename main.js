a047a836195d713a67143459765fd13e9dce5d56
Mohammed Mukhtar <mohammedmuk.fr@gmail.com>
2025-09-23 22:41:43 +0300

    version one

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+// About Us section transition effect
+
+let AboutUsTxtBox = document.querySelector(".About-Us .text-box"),
+    ServicesTxtBox = document.querySelector(".Our-Services .text-box"),
+    VisionTxtBox = document.querySelector(".Our-Vision .text-box");
+
+console.log(window.scrollY);
+
+window.onscroll = () => {
+       console.log(window.scrollY);
+       
+       if (window.scrollY > 540){
+        AboutUsTxtBox.style.opacity = 1;
+        AboutUsTxtBox.style.transform = "translateY(0)";
+       }
+
+       if (window.scrollY > 1190){
+              ServicesTxtBox.style.opacity = 1;
+              ServicesTxtBox.style.transform = "translateX(0)";
+       }
+
+       if (window.scrollY > 1790){
+              VisionTxtBox.style.opacity = 1;
+              VisionTxtBox.style.transform = "translateX(0)";
+       }
+}
+
